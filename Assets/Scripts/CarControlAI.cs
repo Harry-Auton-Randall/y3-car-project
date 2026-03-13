e7e83d508f511b2fd033165c8f72c67cdc1c8376
Harry-Auton-Randall <hautonrandall@gmail.com>
2026-03-13 15:13:25 +0000

    ai-corner-anticipation progress
    AI car now knows the radius, max speed, angle and distance of all of its targetWaypoints. It doesn't act on them yet.

--- Assets/Scripts/CarControlAI.cs
+++ Assets/Scripts/CarControlAI.cs
@@ -18,6 +18,19 @@ public class CarControlAI : MonoBehaviour
 
     Collider[] nextWaypoints;
     Transform[] targetWaypoints;
+
+    Vector3[] waypointTurningEnds;
+    float[] waypointTurningRadii;
+    float[] waypointTurningSpeeds;
+    float[] waypointTurningAngles; //NEW
+    float[] waypointTurningDists; //NEW
+
+    Vector3 carTurningEnd; //NEW
+    float carTurningRadius; //NEW
+    float carTurningSpeed; //NEW
+    float carTurningAngle; //NEW
+    float carTurningDist; //NEW
+
     public int waypointsAhead = 2;
 
     void Awake()
@@ -28,11 +41,11 @@ public class CarControlAI : MonoBehaviour
 
     public void UpdateWaypoint(Collider currentWaypointIn, Collider[] nextWaypointsIn)
     {
-        if (nextWaypoints == null)
+        if (nextWaypoints == null) //start of race / no nextWaypoints
         {
             RecalcWaypoints(nextWaypointsIn);
         }
-        else if (currentWaypointIn.transform == targetWaypoints[0])
+        else if (currentWaypointIn.transform == targetWaypoints[0]) //correct waypoint hit
         {
             nextWaypoints = nextWaypointsIn;
             for (int i=0;i<waypointsAhead - 1; i++)
@@ -41,22 +54,90 @@ public class CarControlAI : MonoBehaviour
             }
             targetWaypoints[waypointsAhead - 1] = FindNextWaypoint
                 (targetWaypoints[waypointsAhead - 2].GetComponent<Waypoint>().nextWaypoints);
+
+            if (waypointsAhead > 2)
+            {
+                for (int i=0; i<waypointsAhead - 2; i++)
+                {
+                    waypointTurningEnds[i] = waypointTurningEnds[i + 1];
+                    waypointTurningRadii[i] = waypointTurningRadii[i + 1];
+                    waypointTurningSpeeds[i] = waypointTurningSpeeds[i + 1];
+                    waypointTurningAngles[i] = waypointTurningSpeeds[i + 1];
+                    waypointTurningDists[i] = waypointTurningSpeeds[i + 1];
+                }
+            }
+
+            waypointTurningEnds[waypointsAhead - 2] = CalculateTurningEnd
+                (targetWaypoints[waypointsAhead - 2], targetWaypoints[waypointsAhead - 1]);
+
+            waypointTurningRadii[waypointsAhead - 2] = CalculateTurningRadius
+                (waypointTurningEnds[waypointsAhead - 2]);
+
+            waypointTurningSpeeds[waypointsAhead - 2] = CalculateTurningSpeed
+                (waypointTurningRadii[waypointsAhead - 2]);
+
+            waypointTurningAngles[waypointsAhead - 2] = CalculateTurningAngle
+                (waypointTurningEnds[waypointsAhead - 2], waypointTurningRadii[waypointsAhead - 2]); //NEW
+
+            if (waypointTurningAngles[waypointsAhead - 2] == 0) //NEW IF/ELSE
+            {
+                waypointTurningDists[waypointsAhead - 2] = waypointTurningEnds[waypointsAhead - 2].z;
+            }
+            else
+            {
+                waypointTurningDists[waypointsAhead - 2] = CalculateTurningDist
+                    (waypointTurningAngles[waypointsAhead - 2], waypointTurningRadii[waypointsAhead - 2]);
+            }
         }
-        else
+        else //incorrect waypoint hit
         {
             RecalcWaypoints(nextWaypointsIn);
         }
     }
+
     void RecalcWaypoints(Collider[] nextWaypointsIn2)
     {
+        //initialises targetWaypoints, for start of race
         targetWaypoints = new Transform[waypointsAhead];
+
+        waypointTurningEnds = new Vector3[waypointsAhead - 1];
+        waypointTurningRadii = new float[waypointsAhead - 1];
+        waypointTurningSpeeds = new float[waypointsAhead - 1];
+        waypointTurningAngles = new float[waypointsAhead - 1]; //NEW
+        waypointTurningDists = new float[waypointsAhead - 1]; //NEW
+
         nextWaypoints = nextWaypointsIn2;
         targetWaypoints[0] = FindNextWaypoint(nextWaypoints);
         for (int i=1; i<waypointsAhead;i++)
         {
-            targetWaypoints[i] = FindNextWaypoint(targetWaypoints[i - 1].GetComponent<Waypoint>().nextWaypoints);
+            targetWaypoints[i] = FindNextWaypoint
+                (targetWaypoints[i - 1].GetComponent<Waypoint>().nextWaypoints);
+        }
+
+        for (int i=0; i<waypointsAhead - 1; i++)
+        {
+            waypointTurningEnds[i] = CalculateTurningEnd
+                (targetWaypoints[i], targetWaypoints[i + 1]);
+
+            waypointTurningRadii[i] = CalculateTurningRadius(waypointTurningEnds[i]);
+
+            waypointTurningSpeeds[i] = CalculateTurningSpeed(waypointTurningRadii[i]);
+
+            waypointTurningAngles[i] = CalculateTurningAngle
+                (waypointTurningEnds[i], waypointTurningRadii[i]); //NEW
+
+            if (waypointTurningAngles[i] == 0) //NEW IF/ELSE
+            {
+                waypointTurningDists[i] = waypointTurningEnds[i].z;
+            }
+            else
+            {
+                waypointTurningDists[i] = CalculateTurningDist
+                    (waypointTurningAngles[i], waypointTurningRadii[i]);
+            }
         }
     }
+
     Transform FindNextWaypoint(Collider[] current)
     {
         if (current.Length == 1)
@@ -67,6 +148,50 @@ public class CarControlAI : MonoBehaviour
         {
             return current[Random.Range(0, current.Length)].transform;
         }
+    }
+
+    Vector3 CalculateTurningEnd(Transform startPos, Transform endPos)
+    {
+        return startPos.InverseTransformPoint(endPos.position);
+    }
+
+    float CalculateTurningRadius(Vector3 localEndPos)
+    {
+        return Mathf.Abs(
+            (Mathf.Pow(localEndPos.x, 2) + Mathf.Pow(localEndPos.z, 2))
+            / (2 * localEndPos.x));
+    }
+
+    float CalculateTurningSpeed(float turnRadius)
+    {
+        return 2.95258f * Mathf.Pow(turnRadius, 0.542118f);
+    }
+
+    float CalculateTurningAngle(Vector3 localEndPos, float turnRadius)
+    {
+        if (turnRadius == Mathf.Infinity)
+        {
+            return 0f;
+        }
+        else
+        {
+            //Finds start and end positions relative to the turning centre
+            localEndPos.x = Mathf.Abs(localEndPos.x);
+            localEndPos.x -= turnRadius;
+            localEndPos.y = 0;
+
+            float tempAngle = Vector3.SignedAngle(-Vector3.right, localEndPos, Vector3.up);
+            if (tempAngle < 0f)
+            {
+                tempAngle += 360f;
+            }
+            return tempAngle;
+        }
+    }
+
+    float CalculateTurningDist(float turnAngle, float turnRadius)
+    {
+        return (Mathf.PI * 2 * turnRadius * (turnAngle / 360f));
     }
 
     void Update()
